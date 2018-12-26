fb91fab5ca27f0554e65e0b8408d6a57bf038b1b
MarkStav <mark.gurianov@gmail.com>
2018-12-26 17:47:35 +0300

    Добавлена функция intInput для считывания целого числа

--- mark.py
+++ mark.py
@@ -1,5 +1,8 @@
 import math
-def solver1 (a,b,c):
+def intInput():
+    a = int(input())
+    return a
+def solver1(a, b, c):
     D = b ** 2 - 4 * a * c
     if D > 0:
         print("Уравнение имеет 2 корня:  ", (- b + math.sqrt(D)) / 2 * a, ' ', (- b - math.sqrt(D)) / 2 * a)
@@ -7,7 +10,9 @@ def solver1 (a,b,c):
         print("Уравнение имеет 1 корнь:   ", (- b + math.sqrt(D)) / 2 * a)
     else:
         print(" Нет решения")
-def solver2 (a,b,c,d):
+
+
+def solver2(a, b, c, d):
     n = 1
     print("Найдем 1 корень с помощью схемы Горнера")
     print('|', a, '|', b, '|', c, '|', d, '|')
@@ -20,7 +25,7 @@ def solver2 (a,b,c,d):
             print('|', a1, '|', b1, '|', c1, '|', d1, '|')
             if d1 == 0:
                 print("Первый корень:  ", n)
-                solver1(a1,b1,c1)
+                solver1(a1, b1, c1)
                 break
             a1 = a
             b1 = a1 * (-n) + b
@@ -31,16 +36,20 @@ def solver2 (a,b,c,d):
                 print("Первый корень:  ", n)
                 solver1(a1, b1, c1)
                 break
+
+
 n += 1
 
-print("[1] - квадратное \n[2] - кубическое")
+print("[1] - квадратное \n[2] - кубическое \n[3] - решение мнгочлена с 4 степенью")
 n = int(input("Выберите уравнение для решения:   "))
 if n == 1:
     print("Введите уравнение вида ax^2+bx+c")
-    a,b,c = int(input()), int(input()), int(input())
-    solver1(a, b,   c)
+    a, b, c = intInput(), intInput(), intInput()
+    solver1(a, b, c)
 elif n == 2:
     
     print("Введите уравнение вида ax^3+bx^2+cx+d")
-    a,b,c,d = int(input()),int(input()),int(input()),int(input())
+    a, b, c, d = intInput(), intInput(), intInput(), intInput()
     solver2(a, b, c, d)
+elif n == 3:
+    a,b,c,d,e = intInput(), intInput(), intInput(), intInput(), intInput()
