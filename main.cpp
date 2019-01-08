b54198384156e0782c7423f7f53481058455153a
MarkStav <mark.gurianov@gmail.com>
2019-01-08 12:13:09 +0300

    Урок 2, Часть 3 "Указателли в функциях

--- main.cpp
+++ main.cpp
@@ -6,12 +6,17 @@ void ChangeData(int &a){
 void ChangePointer(int *a){
     *a = 15;
 }
+int* getPtr(){
+    int a = 9;
+    return &a;
+}
 int main(){
     int a = 5;
     int *b = &a;
     int &c = a;
     ChangeData(a);
     ChangePointer(b);
-    cout<<a<<' '<<b<<' '<<c<<endl;  //15 0x7ffeec9a176c 15
+    cout << a << ' ' << b << ' ' << c << endl;  //15 0x7ffeec9a176c 15
+    int* d = getPtr();
+    cout << d << ' ' << *d << endl;             // является ошибкой но программа запускается, кадый раз выдавая разные адреса
 }
-
