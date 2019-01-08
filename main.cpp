539251c9ef9e85ae34f00c3b35c32e55fa616c42
MarkStav <mark.gurianov@gmail.com>
2019-01-08 11:47:32 +0300

    Урок 2, Часть 2 "Указатели в функциях"

--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@ using namespace std;
 void ChangeData(int &a){
     a = 10;
 }
+void ChangePointer(int *a){
+    *a = 15;
+}
 int main(){
     int a = 5;
     int *b = &a;
     int &c = a;
     ChangeData(a);
-    cout<<a<<' '<<b<<' '<<c<<endl;  //10 << 0x7ffee93e876c << 10
+    ChangePointer(b);
+    cout<<a<<' '<<b<<' '<<c<<endl;  //15 0x7ffeec9a176c 15
 }
+
